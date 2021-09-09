7397414b01ec3d65fb94da0f83a4d973237cab08
MarcusVinicius21 <marcugomes21@hotmail.com>
2021-09-09 17:37:41 -0300

    inclusão de security

---
+++ src/main/java/br/org/generation/lojagames/service/UsuarioService.java
@@ -0,0 +1,106 @@
+package br.org.generation.lojagames.service;
+
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.org.generation.lojagames.model.Usuario;
+import br.org.generation.lojagames.model.UsuarioLogin;
+import br.org.generation.lojagames.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+	public String encoder(String senha) {
+		return encoder.encode(senha);
+
+	}
+
+	public List<Usuario> listarUsuario() {
+		return usuarioRepository.findAll();
+	}
+
+	public Optional<Usuario> buscarUsuarioId(long id) {
+
+		return usuarioRepository.findById(id);
+	}
+
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O usuário já existe", null);
+
+		int idade = Period.between(usuario.getDataNascimento(), LocalDate.now()).getYears();
+
+		if (idade < 18)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O usuário é menor de 18", null);
+
+		usuario.setSenha(encoder(usuario.getSenha()));
+
+		return Optional.of(usuarioRepository.save(usuario));
+
+	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
+
+			int idade = Period.between(usuario.getDataNascimento(), LocalDate.now()).getYears();
+
+			if (idade < 18)
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O Usuário é menor de idade!", null);
+
+			usuario.setSenha(encoder(usuario.getSenha()));
+
+			return Optional.of(usuarioRepository.save(usuario));
+			
+
+		} else {
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "O Usuário não encontrado!", null);
+
+		}
+
+	}
+
+	public Optional<UsuarioLogin> loginUsuario(Optional<UsuarioLogin> usuarioLogin) {
+
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
+
+		if (usuario.isPresent()) {
+
+			if (encoder.matches(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = usuarioLogin.get().getUsuario() + ":" + usuarioLogin.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+
+				usuarioLogin.get().setId(usuario.get().getId());
+				usuarioLogin.get().setNome(usuario.get().getNome());
+				usuarioLogin.get().setSenha(usuario.get().getSenha());
+				usuarioLogin.get().setToken(authHeader);
+
+				return usuarioLogin;
+
+			}
+
+		}
+
+		throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Login invalido, não autorizado!", null);
+	}
+
+}
